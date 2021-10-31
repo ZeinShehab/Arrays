c5a3d5f2090736200f1874b965abd35889930293
ZeinShehab <zeinshehab@outlook.com>
2021-10-31 17:12:52 +0200

    in_rng & out_rng now have default values

--- arrays.py
+++ arrays.py
@@ -8,23 +8,31 @@ def to_str(array: List[any], spacing: int=0) -> str:
     return space.join(str(x) for x in array)
 
 
-def to_str3D(array: List[List[any]], spacing: int=1) -> str:
+def to_str2D(array: List[List[any]], spacing: int=1) -> str:
     return "\n".join(to_str(list(arr), spacing) for arr in array)    
 
 
-def add_char(array: List[any], char: str, count: int=1, interval: int=1, rng: list=[False, False]) -> List[any]:
+def add_char(array: List[any], char: str, count: int=1, interval: int=1, rng: list=[]) -> List[any]:
     char_sub: list = [char for _ in range(count)]
+    max_range: int = int(len(array) / interval)
+    rng_len: int = len(rng)
 
-    if not rng[0] and not rng[1]:
-        rng = range(0, int(len(array)/interval))
+    if rng_len == 0:
         index = 0
+        rng = range(0, max_range)
+    elif rng_len == 1:
+        rng = [rng[0]]
+        index = rng[0]
+        rng[0] = int(rng[0]/interval)
+        rng = range(rng[0], max_range)
     else:
+        rng = [rng[0], rng[1]]
         index = rng[0]
         rng[0], rng[1] = int(rng[0]/interval), int(rng[1]/interval)
         rng = range(rng[0], rng[1])
 
     for _ in rng:
-        for __ in range(len(char_sub)):
+        for _ in range(len(char_sub)):
             array.insert(index, char)
             index += 1
 
@@ -33,10 +41,7 @@ def add_char(array: List[any], char: str, count: int=1, interval: int=1, rng: li
     return array
 
 
-def add_char_in_3D(array: List[List[any]], char: str, count: int=1, interval: List[int]=[1, 1], rng: List[List[int]]=[False, False]) -> List[any]:
-    out_rng = rng[1]
-    in_rng = rng[0]
-
+def add_char_in_2D(array: List[List[any]], char: str, count: int=1, interval: List[int]=[1, 1], in_rng: List[int]=[], out_rng: List[int]=[]) -> List[any]:
     out_interval = interval[1]
     in_interval = interval[0]
 
@@ -45,25 +50,21 @@ def add_char_in_3D(array: List[List[any]], char: str, count: int=1, interval: Li
     if in_interval == 0:
         in_interval = 1
 
-    if len(rng) == 1:
-        pass
+    max_out_range: int = int(len(array)/out_interval)
+    out_rng_len: int = len(out_rng)
 
-    if not in_rng:
-        in_rng = [False, False]
-
-    if not out_rng:        
-        out_rng = range(0, int(len(array)/out_interval))
-        index = 0
+    if out_rng_len == 1:
+        out_rng = range(out_rng[0], max_out_range)
+    elif out_rng_len == 0:
+        out_rng = range(0, max_out_range)
     else:
-        if out_rng[1] == "end":
-            out_rng[1] = len(array)
-
-        index = out_rng[0]
         out_rng[0], out_rng[1] = int(out_rng[0]/out_interval), int(out_rng[1]/out_interval)
         out_rng = range(out_rng[0], out_rng[1])
 
+    index = out_rng[0]
+
     for _ in out_rng:
-        array[index] = add_char(array[index], char, count, in_interval, [in_rng[0], in_rng[1]])
+        array[index] = add_char(array[index], char, count, in_interval, in_rng)
         index += out_interval
 
     return array
